0c8e24c001fd8eff6d053dcc7103b623ecf8dfa8
Emirhan Bilgiç <82952986+emirhanbilgic@users.noreply.github.com>
2023-09-28 13:02:35 +0300

    Create balanced_binary_tree.py

---
+++ balanced_binary_tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def height(root):
+            if not root:
+                return 0
+            return 1 + max(height(root.left), height(root.right))
+        
+        if not root:
+            return True
+        
+        left_height = height(root.left)
+        right_height = height(root.right)
+        return abs(left_height - right_height) <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
